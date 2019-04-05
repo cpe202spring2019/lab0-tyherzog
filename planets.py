f9bb49c6902f68a21f7bc524838b691c50a9b95c
Tyler Herzog <tyler.r.herzog@gmail.com>
2019-04-05 10:24:27 -0700

    added user input and output user weight on differing planets

--- planets.py
+++ planets.py
@@ -1,7 +1,7 @@
 def weight_on_planets():
    # write your code here
-   
-   
-   
-if __name__ == '__main__':
+   person_weight = input('What do you weigh on earth? ') #asks user for their weight
+   person_weight = int(person_weight) #casts the string into an integer
+   print ("\nOn Mars you would weigh", (person_weight*.38), "pounds.\nOn Jupiter you would weigh", (person_weight*2.34), "pounds.") #Prints various weights for different planets
+if __name__ == '__main__': #When called will run function
    weight_on_planets()
